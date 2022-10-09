84da0c209290afb8102a5a4cd1f4633aa37366a8
kennard <kyaxery@gmail.com>
2022-10-09 15:17:18 +0200

    fix bug with config

--- src/getMetrikaInstance.js
+++ src/getMetrikaInstance.js
@@ -1,8 +1,8 @@
 export function getMetrikaInstance(config) {
     var instance = new Ya.Metrika2(config);
-    setMetrikaInstance(instance);
+    setMetrikaInstance(instance, config);
     return instance;
 }
-function setMetrikaInstance(instance) {
+function setMetrikaInstance(instance, config) {
     window["yaCounter".concat(config.id)] = instance;
 }
