2a92ff8b7b847c1a1c42d5dde91bef424ead4206
nWacky <gosha.evtushenko@gmail.com>
2023-11-15 00:49:29 +0300

    build

--- src/getMetrikaInstance.d.ts
+++ src/getMetrikaInstance.d.ts
@@ -1,2 +1,5 @@
 import type { Config, YaMetrika } from "./types.js";
-export declare function getMetrikaInstance(config: Config | Record<string, any>): YaMetrika;
+export declare function getMetrikaInstance(config: Config | Record<string, any>, params: {
+    /** If `true`, try using `Ya.Metrika` first */
+    tryMetrika1: boolean;
+}): YaMetrika;
--- src/helpers.d.ts
+++ src/helpers.d.ts
@@ -2,6 +2,6 @@ import { App } from "vue";
 import type { YaMetrika } from "./types.js";
 export declare function updateConfig(params: Record<string, any>): void;
 export declare function checkConfig(): void;
-export declare function loadScript(callback: () => void, scriptSrc?: "https://mc.yandex.ru/metrika/tag.js"): void;
+export declare function loadScript(callback: () => void, scriptSrc?: import("./types.js").ScriptSrcTy): void;
 export declare function createMetrika(app: App): YaMetrika;
 export declare function startTracking(metrika: YaMetrika): void;
