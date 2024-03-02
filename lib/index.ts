723d0ef9f9a2edb6acbffc00c67675ad10e23cf7
kennard <kyaxery@gmail.com>
2024-03-03 01:09:58 +0200

    added update metrika instance by config

--- lib/index.ts
+++ lib/index.ts
@@ -1,33 +1,42 @@
 /* istanbul ignore file */
-import { App } from "vue";
-import { EmptyYaMetrika } from "./empty-ya.js";
-import {
-  updateConfig,
-  checkConfig,
-  loadScript,
-  createMetrika,
-  startTracking,
-} from "./helpers.js";
+import {App} from 'vue'
+import {EmptyYaMetrika} from './empty-ya.js'
+import {checkConfig, createMetrika, loadScript, startTracking, updateConfig,} from './helpers.js'
 
-import type { Config } from "./types.js";
-import { YaMetrikaObject } from "./ya-object.js";
+import type {Config} from './types.js'
+import {YaMetrikaObject} from './ya-object.js'
 
 let _metrikaInstance = new YaMetrikaObject();
+let _app: App | null = null
 
 export function initYandexMetrika(app: App, options: Config) {
   const emptyMetrika = new EmptyYaMetrika();
   _metrikaInstance.setMetrika(emptyMetrika);
-
-  app.config.globalProperties.$yandexMetrika = _metrikaInstance;
-  updateConfig(options);
-  checkConfig();
-  loadScript(() => {
-    const metrika = createMetrika(app);
-    _metrikaInstance.setMetrika(metrika);
-    startTracking(_metrikaInstance);
-  }, options.scriptSrc);
+  setApp(app)
+  updateYandexMerikaByConfig(options)
 }
 
 export function useYandexMetrika() {
   return _metrikaInstance;
+}
+
+export function updateYandexMerikaByConfig(options: Config) {
+  if (!_app) {
+    throw Error('Update available after init')
+  }
+  _app.config.globalProperties.$yandexMetrika = _metrikaInstance
+  updateConfig(options)
+  checkConfig()
+  loadScript(() => {
+    if (!_app) {
+      throw Error('Update available after init')
+    }
+    const metrika = createMetrika(_app)
+    _metrikaInstance.setMetrika(metrika)
+    startTracking(_metrikaInstance)
+  }, options.scriptSrc)
+}
+
+export function setApp(app: App) {
+  _app = app
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import { EmptyYaMetrika } from "./empty-ya.js";
-import { updateConfig, checkConfig, loadScript, createMetrika, startTracking, } from "./helpers.js";
-import { YaMetrikaObject } from "./ya-object.js";
+import { EmptyYaMetrika } from './empty-ya.js';
+import { checkConfig, createMetrika, loadScript, startTracking, updateConfig, } from './helpers.js';
+import { YaMetrikaObject } from './ya-object.js';
 let _metrikaInstance = new YaMetrikaObject();
+let _app = null;
 export function initYandexMetrika(app, options) {
     const emptyMetrika = new EmptyYaMetrika();
     _metrikaInstance.setMetrika(emptyMetrika);
-    app.config.globalProperties.$yandexMetrika = _metrikaInstance;
+    setApp(app);
+    updateYandexMerikaByConfig(options);
+}
+export function useYandexMetrika() {
+    return _metrikaInstance;
+}
+export function updateYandexMerikaByConfig(options) {
+    if (!_app) {
+        throw Error('Update available after init');
+    }
+    _app.config.globalProperties.$yandexMetrika = _metrikaInstance;
     updateConfig(options);
     checkConfig();
     loadScript(() => {
-        const metrika = createMetrika(app);
+        if (!_app) {
+            throw Error('Update available after init');
+        }
+        const metrika = createMetrika(_app);
         _metrikaInstance.setMetrika(metrika);
         startTracking(_metrikaInstance);
     }, options.scriptSrc);
 }
-export function useYandexMetrika() {
-    return _metrikaInstance;
+export function setApp(app) {
+    _app = app;
 }
